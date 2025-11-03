2fdc0e8417bf8dd9e0aefb4e8d13095b01091048
adipopeterson21-create <adipopeterson21@gmail.com>
2025-11-03 20:59:32 +0300

    Add CORS support to the Express app

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,3 +4,4 @@ const express = require('express');
 const app = express();
 app.use('/webhook', require('./routes/webhook'));
 app.listen(5000, ()=>console.log('listening'));
+const cors = require('cors'); app.use(cors({ origin: '*' }));
