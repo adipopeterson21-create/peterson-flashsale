02e9200b15c18331aaab2506c31e7fb2187f450f
adipopeterson21-create <adipopeterson21@gmail.com>
2025-11-03 23:05:32 +0300

    Delete backend/src directory

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-
-require('dotenv').config();
-const express = require('express');
-const app = express();
-app.use('/webhook', require('./routes/webhook'));
-app.listen(5000, ()=>console.log('listening'));
-const cors = require('cors'); app.use(cors({ origin: '*' }));
--- backend/src/routes/webhook.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const stripeWebhook = require('../controllers/webhook');
-
-// Stripe webhook endpoint
-router.post('/stripe', express.raw({type: 'application/json'}), stripeWebhook.handle);
-
-module.exports = router;
